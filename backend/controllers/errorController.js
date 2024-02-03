07ec9360494b8b6370bc8a70f4657aa92dfc7fd2
Muzammil Malik <abdmalik1593@gmail.com>
2024-02-03 12:33:44 +0530

    changes

--- backend/controllers/errorController.js
+++ backend/controllers/errorController.js
@@ -14,28 +14,37 @@ function sendProdError(err, res) {
   });
 }
 
-function handleDuplicateField(err, res) {
+function handleDuplicateFieldError(err, res) {
   console.log(err.message);
   res.status(400).json({
     status: "fail",
     message: `Duplicate field found: ${Object.keys(err.keyPattern)[0]}`,
   });
 }
+
+function handleValidatorError(err, res) {
+  res.status(400).json({
+    status: "fail",
+    message: err.message,
+  });
+}
+
 console.log(process.env.NODE_ENV);
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
+  console.log("Error name: ", err.name);
   if (process.env.NODE_ENV === "development") {
-    // const error = Object.create(err);
-    let error = JSON.parse(JSON.stringify(err));
-    console.log(error);
-    // console.log(error);
-    // console.log(err);
-    if (error.code === 11000) {
-      return handleDuplicateField(error, res);
-    }
     return sendDevError(err, res);
   } else if (process.env.NODE_ENV === "production") {
+    const error = Object.create(err);
+    console.log(error);
+    if (error.code === 11000) {
+      return handleDuplicateFieldError(error, res);
+    }
+    if (error.name === "ValidatorError") {
+      return handleValidatorError(error, res);
+    }
     return sendProdError(err, res);
   }
 };
